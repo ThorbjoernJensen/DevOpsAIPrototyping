24cc4ca5d5e24f925a3d2f1a892724db8570c015
Thorbjørn Jensen <thorbjorn@tech-logic.dk>
2023-11-15 23:16:03 +0100

    Added working AI client to AIFetcher

--- FunctionApp/AIFetcher/Function1.cs
+++ FunctionApp/AIFetcher/Function1.cs
@@ -7,7 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Net.Security;
+using Azure;
+using Azure.AI.OpenAI;
+using System.Text;
 
 namespace AIFetcher
 {
@@ -20,39 +22,39 @@ namespace AIFetcher
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-           
-            return new OkObjectResult("SslClientHelloInfo");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string input = data?.input;
+
+            //// Note: The Azure OpenAI client library for .NET is in preview.
+            //// Install the .NET library via NuGet: dotnet add package Azure.AI.OpenAI --version 1.0.0-beta.5
+
+
+            string endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+            string key = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY");
+
+            // Enter the deployment name you chose when you deployed the model.
+            string engine = "gpt-35-turbo";
+
+            OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+            var chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                Messages =
+    {
+        new ChatMessage(ChatRole.System, "You are a helpful assistant."),
+        new ChatMessage(ChatRole.User, $"{input}"),
+
+    },
+                MaxTokens = 100
+            };
+
+            Response<ChatCompletions> response = client.GetChatCompletions(
+                deploymentOrModelName: "gpt-35-turbo",
+                chatCompletionsOptions);
+
+            Console.WriteLine(response.Value.Choices[0].Message.Content);
+
+            return new OkObjectResult(response.Value.Choices[0].Message.Content);
         }
     }
 }
-
-
-//// Note: The Azure OpenAI client library for .NET is in preview.
-//// Install the .NET library via NuGet: dotnet add package Azure.AI.OpenAI --version 1.0.0-beta.5
-//using Azure;
-//using Azure.AI.OpenAI;
-
-//OpenAIClient client = new OpenAIClient(
-//  new Uri("https://oai-devopsaiprototyping.openai.azure.com/"),
-//  new AzureKeyCredential(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")));
-
-//Response<ChatCompletions> responseWithoutStream = await client.GetChatCompletionsAsync(
-//"gpt-35-turbo",
-//new ChatCompletionsOptions()
-//{
-//    Messages =
-//  {
-//      new ChatMessage(ChatRole.System, @"You are an AI assistant that helps people find information."),
-
-//  },
-//    Temperature = (float)0.7,
-//    MaxTokens = 800,
-
-
-//    NucleusSamplingFactor = (float)0.95,
-//    FrequencyPenalty = 0,
-//    PresencePenalty = 0,
-//});
-
-
-//ChatCompletions response = responseWithoutStream.Value;
