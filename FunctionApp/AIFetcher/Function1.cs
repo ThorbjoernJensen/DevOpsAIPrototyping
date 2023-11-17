ad40e5793de6248d599ed1e492723db7921d6f3c
Thorbjørn Jensen <thorbjorn@tech-logic.dk>
2023-11-17 11:11:31 +0100

    added prompts to fa to provide workitem array in json format

--- FunctionApp/AIFetcher/Function1.cs
+++ FunctionApp/AIFetcher/Function1.cs
@@ -37,24 +37,64 @@ namespace AIFetcher
             string engine = "gpt-35-turbo";
 
             OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+            //        var chatCompletionsOptions = new ChatCompletionsOptions()
+            //        {
+            //            Messages =
+            //{
+            //    new ChatMessage(ChatRole.System, "You are an assistant that only answers with azure devops workitems in json format."),
+            //    new ChatMessage(ChatRole.System, "You do not reply with anything but json, as the json is to be parsed"),
+            //    new ChatMessage(ChatRole.System, "You provide arrays of workitem objects following this schema {\r\n    \"type\": \"array\",\r\n    \"items\": {\r\n        \"type\": \"object\",\r\n        \"properties\": {\r\n            \"from\": {},\r\n            \"op\": {\r\n                \"type\": \"string\"\r\n            },\r\n            \"path\": {\r\n                \"type\": \"string\"\r\n            },\r\n            \"value\": {\r\n                \"type\": \"string\"\r\n            }\r\n        },\r\n        \"required\": [\r\n            \"from\",\r\n            \"op\",\r\n            \"path\",\r\n            \"value\"\r\n        ]\r\n    }\r\n} ."),
+            //    new ChatMessage(ChatRole.User, $"{input}"),
+
+            //},
+            //            MaxTokens = 1000
+            //        };
+
             var chatCompletionsOptions = new ChatCompletionsOptions()
             {
                 Messages =
     {
-        new ChatMessage(ChatRole.System, "You are a helpful assistant."),
+        new ChatMessage(ChatRole.System, "You are an assistant that provides Azure DevOps work items in a simple JSON format."),        
+        //new ChatMessage(ChatRole.System, "Each work item should be a array consisting of objects with fields 'op',  'title', and 'description'. An example of an valid object is {\"id\": 1, \"title\": \"Sample Task\", \"description\": \"This is a sample description.\"} "),
+        new ChatMessage(ChatRole.System, "Format each work item as an array containing at least one object. Each object must have the keys 'from', 'op', 'path', and 'value'. "),
+        new ChatMessage(ChatRole.System, "For the Title object Set 'from' to null, 'op' to 'add', and 'path' to '/fields/System.Title'. The 'value' should contain the task's title. For example: [{\"from\": null, \"op\": \"add\", \"path\": \"/fields/System.Title\", \"value\": \"Sample task\"}]"),
+        new ChatMessage(ChatRole.System, "For the Description object Set 'from' to null, 'op' to 'add', and 'path' to '/fields/System.Description'. The 'value' should contain the task's description."),
+        new ChatMessage(ChatRole.System, "You should provide each workitem with an Title and corresponding description"),       
+        new ChatMessage(ChatRole.System, "the arrays containing the individual workitems should be collected in an array using square brackets [] and given the name workitems. An example of an valid array output is \"workitems\": [[{object1}],[{object2}], etc..]"),
+        new ChatMessage(ChatRole.System, "the data should be enclosed in {} as in JSON format. and example is {\"workitems\":[[{object1}],[{object2}]}"),
         new ChatMessage(ChatRole.User, $"{input}"),
-
     },
-                MaxTokens = 100
+                MaxTokens = 1000
             };
+
+
+
 
             Response<ChatCompletions> response = client.GetChatCompletions(
                 deploymentOrModelName: "gpt-35-turbo",
                 chatCompletionsOptions);
 
+
+
+            Console.WriteLine("Response content before json extract");
             Console.WriteLine(response.Value.Choices[0].Message.Content);
 
-            return new OkObjectResult(response.Value.Choices[0].Message.Content);
+            
+            try
+            {
+                var jsonResponseObject = JSONExtractor.ExtractJson(response.Value.Choices[0].Message.Content);
+                Console.WriteLine("Response content after json extract");
+                return new OkObjectResult(jsonResponseObject);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return new OkObjectResult("json validation failed");
+            }
+
+
+
+
+            
         }
     }
 }
---
+++ FunctionApp/AIFetcher/JSONExtractor.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIFetcher
+{
+    internal class JSONExtractor
+    {
+        public static JObject ExtractJson(string response)
+        {
+            int startIndex = response.IndexOf('{');
+            int endIndex = response.LastIndexOf('}') + 1; // +1 to include the closing brace
+
+            string jsonPart = "";
+            try
+            {
+                if (startIndex != -1 && endIndex != -1 && endIndex > startIndex)
+                {
+                    jsonPart = response.Substring(startIndex, endIndex - startIndex);
+                    // Now jsonPart contains the JSON string
+                    
+
+                }
+                else
+                {
+                    // Handle cases where the braces are not found
+                }
+
+            }
+            catch (JsonReaderException)
+            {
+                throw new InvalidOperationException("Invalid JSON data.");
+            }
+            return JObject.Parse(jsonPart);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
